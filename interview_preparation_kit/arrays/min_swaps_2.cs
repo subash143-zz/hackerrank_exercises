af14e2187053274b7661d43327a07baf393b9570
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-03-20 21:14:04 -0500

    min_swaps_2

---
+++ interview_preparation_kit/arrays/min_swaps_2.cs
@@ -0,0 +1,52 @@
+// https://www.hackerrank.com/challenges/minimum-swaps-2/problem?h_l=interview&playlist_slugs%5B%5D%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D%5B%5D=arrays&isFullScreen=true
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the minimumSwaps function below.
+    static int minimumSwaps(int[] arr) {
+      int count = 0;
+      int i = 0;
+      while(i < arr.Count()){
+        int index = arr[i] - 1;
+        if(arr[i] != arr[index]){
+          int temp = arr[i];
+          arr[i] = arr[index];
+          arr[index] = temp;
+          count++;
+        }
+        else{
+          i++;
+        }
+      }
+      return count;
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine());
+
+        int[] arr = Array.ConvertAll(Console.ReadLine().Split(' '), arrTemp => Convert.ToInt32(arrTemp))
+        ;
+        int res = minimumSwaps(arr);
+
+        textWriter.WriteLine(res);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
