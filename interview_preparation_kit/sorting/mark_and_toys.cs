8c50bc38b7fe5aa279d62131996bd9c526b9b4f1
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-20 17:38:06 -0500

    Mark and Toys

---
+++ interview_preparation_kit/sorting/mark_and_toys.cs
@@ -0,0 +1,58 @@
+// https://www.hackerrank.com/challenges/mark-and-toys/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the maximumToys function below.
+    static int maximumToys(int[] prices, int k) {
+      Array.Sort(prices);
+      int retVal = 0;
+      int sum = 0;
+      foreach(int price in prices){
+        if(sum + price > k){
+          return retVal;
+        }
+        else if(sum + price == k){
+          return ++retVal;
+        }
+        else{
+          sum += price;
+          retVal++;
+        }
+      }
+      return retVal;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        string[] nk = Console.ReadLine().Split(' ');
+
+        int n = Convert.ToInt32(nk[0]);
+
+        int k = Convert.ToInt32(nk[1]);
+
+        int[] prices = Array.ConvertAll(Console.ReadLine().Split(' '), pricesTemp => Convert.ToInt32(pricesTemp))
+        ;
+        int result = maximumToys(prices, k);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
