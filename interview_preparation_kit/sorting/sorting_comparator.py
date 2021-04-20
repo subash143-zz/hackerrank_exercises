79067640adcfc9c8297c2b541890772eb3d82500
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-20 18:01:54 -0500

    Sorting Comparator

---
+++ interview_preparation_kit/sorting/sorting_comparator.py
@@ -0,0 +1,26 @@
+# https://www.hackerrank.com/challenges/ctci-comparator-sorting/problem?h_l=interview&playlist_slugs%5B%5D=interview-preparation-kit&playlist_slugs%5B%5D=sorting
+
+from functools import cmp_to_key
+class Player:
+    def __init__(self, name, score):
+        self.name = name
+        self.score = score
+    def __repr__(self):
+        return self.name
+      
+    def comparator(a, b):
+        if(a.score > b.score):
+          return -1
+        elif a.score < b.score:
+          return 1
+        else:
+          i = 0
+          while i < len(a.name) and i < len(b.name):
+            if a.name[i] > b.name[i]:
+              return 1
+            elif(a.name[i] < b.name[i]):
+              return -1;
+            i = i + 1
+        
+          return 1 if len(a.name) > len(b.name) else -1
+n = int(input())
