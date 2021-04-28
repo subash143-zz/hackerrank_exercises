1f82beb9894a932da94a77a620a9379d1b931267
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-27 23:11:17 -0500

    Subarray Division

---
+++ algorithms/implementation/subarray_division.cs
@@ -0,0 +1,54 @@
+// https://www.hackerrank.com/challenges/the-birthday-bar/problem?utm_campaign=challenge-recommendation&utm_medium=email&utm_source=24-hour-campaign&isFullScreen=true
+// Subarrary Division
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the birthday function below.
+    static int birthday(List<int> s, int d, int m) {
+      var retVal = 0;
+      for(int i = 0; i < s.Count() - m + 1; i++){
+        Console.WriteLine(i);
+        if(s.Skip(i).Take(m).Sum() == d){
+          retVal ++;
+        }
+      }
+      
+      return retVal;
+
+    }
+
+    static void Main(string[] args) {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> s = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(sTemp => Convert.ToInt32(sTemp)).ToList();
+
+        string[] dm = Console.ReadLine().TrimEnd().Split(' ');
+
+        int d = Convert.ToInt32(dm[0]);
+
+        int m = Convert.ToInt32(dm[1]);
+
+        int result = birthday(s, d, m);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
