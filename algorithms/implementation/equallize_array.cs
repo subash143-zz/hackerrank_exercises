d4bf8229c8eec5c7b9f186eaf5d7e81724f6309c
subashkharel146@gmail.com <subashkharel146@gmail.com>
2021-04-27 22:58:41 -0500

    Equalize Array

---
+++ algorithms/implementation/equallize_array.cs
@@ -0,0 +1,53 @@
+// https://www.hackerrank.com/challenges/equality-in-a-array/problem?isFullScreen=true
+// Equalize array
+
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Result
+{
+
+    /*
+     * Complete the 'equalizeArray' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int equalizeArray(List<int> arr)
+    {
+      var dict = arr.GroupBy(y=>y).ToDictionary(x=>x.Key, x=>x.Count());
+      return arr.Count() - dict.Keys.Select(x=>dict[x]).Max();
+    }
+
+}
+
+class Solution
+{
+    public static void Main(string[] args)
+    {
+        TextWriter textWriter = new StreamWriter(@System.Environment.GetEnvironmentVariable("OUTPUT_PATH"), true);
+
+        int n = Convert.ToInt32(Console.ReadLine().Trim());
+
+        List<int> arr = Console.ReadLine().TrimEnd().Split(' ').ToList().Select(arrTemp => Convert.ToInt32(arrTemp)).ToList();
+
+        int result = Result.equalizeArray(arr);
+
+        textWriter.WriteLine(result);
+
+        textWriter.Flush();
+        textWriter.Close();
+    }
+}
